b484a9de182e5ae6121db72e44b6f8a4c9a40b9d
B BHARATH KUMAR REDDY <bharathboienipalli@gmail.com>
2025-06-18 07:54:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
+function numberOfBooksRead(library) {
+  if (!Array.isArray(library)) {
+    throw new Error("Input must be an array of books.");
+  }
+	
+  let count = 0;
+  for (let book of library) {
+    if (book.readingStatus === true) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
 const library = [
   {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
+    author: 'Bill Gates',
+    title: 'The Road Ahead',
+    readingStatus: true
   },
   {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
+    author: 'Steve Jobs',
+    title: 'Walter Isaacson',
+    readingStatus: true
   },
   {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
+    author: 'Suzanne Collins',
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    readingStatus: false
+  }
 ];
 
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
-// Do not change the code below
+console.log(numberOfBooksRead(library)); 
